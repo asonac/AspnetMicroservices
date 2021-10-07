c121e9632132e850fe50e195ab1b42e645964085
asonac <wesley.canosa@gmail.com>
2021-10-07 15:48:38 -0300

    masket microservices devlopted

---
+++ src/Services/Basket/Basket.API/Controllers/BasketControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController, Route("api/v1/[controller]")]
+    public class BasketControllerBase
+    {
+        private readonly IBasketRepository _repository;
+    }
+}
